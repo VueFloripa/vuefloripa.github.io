b9bda9ff0f13bdc284322a6b2eaedcb3b2a5d487
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2019-05-30 14:55:13 +0200

    :construction_worker: Adding Travis CI for deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Setup Travis git user
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+# Go to `dist` folder
+cd ./src/.vuepress/dist/
+
+# Commit `dist` files
+git init
+git add -A
+git commit -m "Deploy $TRAVIS_COMMIT"
+
+# Push to GitHub
+git push -f https://${GITHUB_TOKEN}@github.com:vuefloripa/vuefloripa.github.io.git master
